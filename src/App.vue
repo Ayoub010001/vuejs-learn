8882ad53027a52c8e4504a7cf3b481d43808a825
Ayoub Ritoub <aritoub@smb.sqli.com>
2025-03-26 12:46:45 +0000

    simple to-do with view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup>
 import { ref } from 'vue'
 
-// component logic
-// declare some reactive state here.
+const allTasks = ref([]);
+const newTask = ref('');
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    allTasks.value.push(newTask.value.trim());
+    newTask.value = '';
+  }
+};
+const deleteTask = (index) => {
+  allTasks.value.splice(index, 1);
+};
 </script>
 
 <template>
-  <h1>Make me dynamic!</h1>
+  <h1>Simplest To do app ever!</h1>
+  <br>
+  <div>
+    <input v-model="newTask" type="text" placeholder="Enter a task" />
+    <button @click="addTask">Add</button>
+  </div>
+  <br>
+  <div>
+    <ul>
+      <li v-for="(task, index) in allTasks" :key="index">
+        {{ task }}
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
